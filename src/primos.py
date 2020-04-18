d4e53ecb4513b73e6bddd7d256d640307d858d63
pedroooliveira1914 <ph.nboliveira@gmail.com>
2020-04-17 22:55:23 -0300

    ac02

---
+++ src/primos.py
@@ -0,0 +1,20 @@
+def primos():
+    limite = 100
+
+    c = 1
+    p = 1
+    numero = 3
+
+    primos = "2,"
+
+    while p < limite:
+        ehprimo = 1
+        for i in range(2, numero):
+            if numero % i == 0:
+                ehprimo = 0
+                break
+        if (ehprimo):
+            primos = primos + str(numero) + ","
+            p += 1 
+            numero += 1
+    return
---
+++ src/testes.py
@@ -0,0 +1,1 @@
+#aqui serao feito testes
